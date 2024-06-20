dc0603810f38dd2e729d0e849f7f9a7f6c2f50c8
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-06-21 02:09:10 +0530

    checking mail service

--- forms/mailJSsender.js
+++ forms/mailJSsender.js
@@ -1,9 +1,9 @@
-function sendMail() {
+async function sendMail() {
     let params ={
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         message: document.getElementById('message').value,
         subject: document.getElementById('subject').value
     }
-    emailjs.send('service_ygwl1pl','template_nftjmfb',params).then(alert("message sent"));
+    await emailjs.send('service_ygwl1pl','template_nftjmfb',params).then(alert("message sent"));
 }
